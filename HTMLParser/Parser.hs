54ea35898279f1ec300a0c07de2cd73a250ca737
mcardoff <mcardiff0321@gmail.com>
2020-07-12 23:09:17 -0500

    cleaned parser with comments

--- HTMLParser/Parser.hs
+++ HTMLParser/Parser.hs
@@ -4,11 +4,17 @@ import Data.Char
 import Data.Functor
 import Control.Applicative
 
+-- What is actually gonna be parsed, HTML!
+
 data HTML = Element {elemName :: String, elemContent :: [HTML]}
           | Attribute {attribName :: String, attribVal :: String}
           | Text {textContent :: String} deriving Show
 
+-- Could be expanded further, but this works just fine
+
 newtype Parser a = Parser {parse :: String -> Maybe (String, a)}
+
+-- instances for Parser, Functor, Applicative, Alternative, and Monad
 
 instance Functor Parser where
     fmap f (Parser p) = Parser $ \s -> do
@@ -33,71 +39,98 @@ instance Monad Parser where
                                         Just (s', x) -> parse (f x) s'
                                         _ -> Nothing
 
+-- General Parser functions --
 
+-- Parses the given character
 charP :: Char -> Parser Char
 charP c = Parser $ \s -> case s of
                         (x:xs) | x == c -> Just (xs,x)
                         _ -> Nothing
 
+-- Parses the given string as a sequence of character parseres
 stringP :: String -> Parser String
 stringP = traverse charP
 
-stringLiteral :: Parser String
-stringLiteral = betweenP (charP '"') (charP '"') (spanP (/= '"'))
 
+-- Parses an identifier between quotes
+stringLiteral :: Parser String
+stringLiteral = betweenC '"' '"' (spanP (/= '"'))
+
+-- Same as span for regular strings
 spanP :: (Char -> Bool) -> Parser String
 spanP p = Parser $ \s -> let (b,a) = span p s in Just (a,b)
 
+-- Same as before but fails on empty
 spanP' :: (Char -> Bool) -> Parser String
 spanP' p = Parser $ \s -> let (b,a) = span p s in
                           case b of
                             [] -> Nothing
                             _ -> Just (a,b)
 
+-- Parses the third parser in between the first two
 betweenP :: Parser a -> Parser b -> Parser c -> Parser c
 betweenP l r p = l *> p <* r
 
+-- Between, but specifically for chars
+betweenC :: Char -> Char -> Parser a -> Parser a
+betweenC l r = betweenP (charP l) (charP r)
+
+-- Parses something between whitespace
+btws :: Parser a -> Parser a
+btws = betweenP ws ws
+
+-- Parses all ws
 ws :: Parser String
 ws = spanP isSpace
 
-btwSpace :: Parser a -> Parser a
-btwSpace = betweenP ws ws
-
-sepBy :: Parser a -> Parser b -> Parser [b] -- Not used int HTML
+-- Parses elem separated by sep
+sepBy :: Parser a -> Parser b -> Parser [b] -- Not used in HTML
 sepBy sep elem = ((:) <$> elem <*> many (sep *> elem)) <|> pure []
 
+-- Same as stringLiteral but uses ws
 token :: Parser String
-token = betweenP ws ws $ spanP isLetter
+token = btws $ spanP isLetter
 
+-- HTML Utility Funcs --
+
+-- parses open tag, but not closing brace
 openTag :: Parser String
 openTag = ws *> charP '<' *> token
 
+-- Parses open tag with no attributes
+anyOpen :: Parser String
 anyOpen = ws *> charP '<' *> spanP isLetter <* charP '>' <* ws
 
+-- Parses closing tag given a name for the tag
 closeTag :: String -> Parser String
 closeTag s = ws *> stringP "</" *> stringP s <* ws <* charP '>'
 
+-- Parses any closing tag
 anyClose :: Parser String
 anyClose = ws *> stringP "</" *> spanP isLetter <* ws <* charP '>'
 
+-- Parses many anyClose, defaults to empty
 manyClose :: Parser [String]
 manyClose = many anyClose
 
+-- Parses the Beginning of the html with doctype in it
 htmlOpening :: Parser String
 htmlOpening = stringP "<!DOCTYPE html>" $> ""
 
--- Monadic Parsing (gross)
+-- Monadic Parsing (gross) --
 
+-- Parses an element, ensures proper HTML formatting
 htmlElem :: Parser HTML
 htmlElem = do
   name <- openTag
   attribs <- many attribP
-  btwSpace $ charP '>'
+  btws $ charP '>'
   content <- many $ htmlElem <|> htmlText
   ws
   closeTag name
   return $ Element name (attribs++content)
 
+-- Parses attributes of an element
 htmlAttrib :: Parser HTML
 htmlAttrib = do
   name <- token
@@ -105,29 +138,36 @@ htmlAttrib = do
   val <- stringLiteral
   return $ Attribute name val
 
+-- Parses text inside an element
 htmlText :: Parser HTML
 htmlText = textP -- Same as applicative
 
+-- Puts it all together
 htmlP :: Parser HTML
 htmlP = htmlOpening *> htmlElem
 
--- Applicative Parsing
+-- Applicative Parsing --
 
-attribP :: Parser HTML
-attribP = Attribute <$> (token <* charP '=') <*> stringLiteral
-
-textP :: Parser HTML
-textP = Text <$> betweenP ws ws (spanP' (\x -> (x/='<')&&(x/='>')))
-
+-- Parses Element recursively
 elemP :: Parser HTML
 elemP = (\x y z -> Element x $ y++z)
         <$> openTag
         <*> many attribP <* charP '>'
         <*> many (elemP <|> textP) <* manyClose
 
+-- Parses attribute of an element
+attribP :: Parser HTML
+attribP = Attribute <$> (token <* charP '=') <*> stringLiteral
+
+-- Parses text content of an element
+textP :: Parser HTML
+textP = Text <$> betweenP ws ws (spanP' (\x -> (x/='<')&&(x/='>')))
+
+-- Shoves it all together
 html :: Parser HTML
 html = htmlOpening *> elemP
 
+-- Examples --
 htmlDoc2 = "<!DOCTYPE html>\
            \<html>\
            \  <head></head>\
@@ -168,6 +208,7 @@ htmlDoc4 = "<!DOCTYPE html>\
            \  </body>\
            \</html>"
 
+-- Use This to test if the parser succedded or failed --
 execParser :: Parser a -> String -> a
 execParser p s = case parse p s of
                    Just ("", y) -> y
